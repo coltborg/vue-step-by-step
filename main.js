e386a68b4303f92483acf91d73304d18bc6c5dba
legendofcolt <coltborg@gmail.com>
2017-07-22 14:17:14 -0500

    Leason 8

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-import Vue from 'vue';
+Vue.component('task-list', {
+  template: `
+    <ul>
+      <task v-for="task in tasks" key="task.task">{{ task.task }}</task>
+    </ul>
+  `,
+  data() {
+    return {
+      tasks: [
+        { task: 'Go to the store', complete: true },
+        { task: 'Go to the email', complete: false },
+        { task: 'Go to the farm', complete: true },
+        { task: 'Go to work', complete: false },
+      ],
+    };
+  },
+});
+
+Vue.component('task', {
+  template: '<li><slot></slot></li>'
+});
 
 new Vue({
   el: '#root'
