ba943cb5aef9c3e51f9b232bd578ad81f12a1f80
legendofcolt <coltborg@gmail.com>
2017-07-22 14:35:52 -0500

    Leason 9

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import Vue from 'vue';
+Vue.component('xi-message', {
+  props: ['title', 'body'],
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+  template: `
+    <article class="message is-primary" v-show="isVisible">
+      <div class="message-header">
+        {{ title }}
+        <button type="button" class="close" @click="isVisible = false">X</button>
+      </div>
+      <div class="message-body">
+        {{ body }}
+      </div>
+    </article>
+  `,
+});
 
 new Vue({
   el: '#root'
