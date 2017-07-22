00469042f9f088965103f3d30aa09bb4fc2ff260
legendofcolt <coltborg@gmail.com>
2017-07-22 14:46:51 -0500

    Leason 10

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-import Vue from 'vue';
+Vue.component('xi-modal', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <slot></slot>
+        </div>
+      </div>
+      <button class="modal-close is-large" @click="$emit('close')"></button>
+    </div>
+  `,
+});
 
 new Vue({
-  el: '#root'
+  el: '#root',
+  data: {
+    showModal: false,
+  },
 });
