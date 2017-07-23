c913eedcad073e74b97f6a2f50a39b25a4832afe
legendofcolt <coltborg@gmail.com>
2017-07-23 09:27:58 -0500

    Leason 13

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-import Vue from 'vue';
+window.Event = new class {
+  constructor() {
+    this.vue = new Vue();
+  }
+  fire(event, data = null) {
+    this.vue.$emit(event, data);
+  }
+  listen(event, callback) {
+    this.vue.$on(event, callback);
+  }
+};
+
+Vue.component('xi-coupon', {
+  template: `<input class="input" placeholder="Enter your coupon code" @blur="onCouponApplied">`,
+  methods: {
+    onCouponApplied() {
+      Event.fire('applied');
+    },
+  },
+});
 
 new Vue({
-  el: '#root'
+  el: '#root',
+  data: {
+    couponApplied: false,
+  },
+  methods: {},
+  created() {
+    Event.listen('applied', () => alert('handling it ✋'));
+  },
 });
