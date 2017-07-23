aac249deb61e3d9aa39d72ec7a2302bf1d876a0e
legendofcolt <coltborg@gmail.com>
2017-07-23 12:35:03 -0500

    Leason 16
    Add vue-loader and webpack

---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .box {
+    background: #e3e3e3;
+    border: 1px solid #e5e5e5;
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+</style>
