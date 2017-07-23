489d643381122f4f1e408fc7755a2b4f1ae7c318
legendofcolt <coltborg@gmail.com>
2017-07-22 21:01:39 -0500

    Leason 11

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-import Vue from 'vue';
+Vue.component('coupon', {
+  template: `
+    <input class="input" type="text" placeholder="Enter your coupon code" @blur="onCouponApplied">
+  `,
+  methods: {
+    onCouponApplied() {
+      this.$emit('applied');
+      console.log('teaw');
+    },
+  },
+});
 
 new Vue({
-  el: '#root'
+  el: '#root',
+  data: {
+    couponApplied: false,
+  },
+  methods: {
+    onCouponApplied() {
+      this.couponApplied = true;
+    },
+  },
 });
